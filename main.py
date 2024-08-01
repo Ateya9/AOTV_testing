56d75b0f10cf20fef3791d56075734d198c04e96
Mark Fish <mark.fish272@gmail.com>
2024-08-01 16:01:45 +0800

    Separated generating a temple and running a temple into their own functions. Changed the input parameters for running a temple, supplying the incursions per area and area count have been replaced with just supplying whether Artefacts of the Vaal is allocated or not.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from random import sample
+from random import sample, randint
 from collections import Counter
 
 
-def gen_new_temple_run(incurs_per_area: int = 3, num_areas: int = 4) -> list:
-    num_temple_rooms = 11
+def gen_temple(num_temple_rooms: int = 11) -> list:
     temple: list[int] = [0] * num_temple_rooms
-    for i in sample(range(num_temple_rooms), 7):  # Set up 7 random rooms to be tier 1 already.
-        temple[i] += 1
+    for room_num in sample(range(num_temple_rooms), 7):  # Set up 7 random rooms to be tier 1 already.
+        temple[room_num] += 1
+    return temple
+
+
+def run_temple_always_upgrade(temple: list, aotv: bool = False, rr: bool = False) -> list:
+    num_temple_rooms = len(temple)
+    incurs_per_area, num_areas = (4, 3) if aotv else (3, 4)
     for _ in range(num_areas):
         available_rooms = [ele for ele in range(num_temple_rooms) if temple[ele] < 3]  # Rooms that aren't maxed.
         for i in sample(available_rooms, incurs_per_area):
@@ -17,12 +22,12 @@ def gen_new_temple_run(incurs_per_area: int = 3, num_areas: int = 4) -> list:
 if __name__ == '__main__':
     temple_room_level_totals = Counter()
     for i in range(100000):
-        temple_room_level_totals += Counter(gen_new_temple_run())
+        temple_room_level_totals += Counter(run_temple_always_upgrade(gen_temple()))
     vanilla_t3_room_ratio = round(temple_room_level_totals[3] / temple_room_level_totals.total(), 4)
     print(f"Ratio of t3 rooms WITHOUT Artefacts of the Vaal: {vanilla_t3_room_ratio}")
 
     temple_room_level_totals = Counter()
     for i in range(100000):
-        temple_room_level_totals += Counter(gen_new_temple_run(4, 3))
+        temple_room_level_totals += Counter(run_temple_always_upgrade(gen_temple(), True))
     aotv_t3_room_ratio = round(temple_room_level_totals[3] / temple_room_level_totals.total(), 4)
     print(f"Ratio of t3 rooms WITH Artefacts of the Vaal: {aotv_t3_room_ratio}")
